0b99faa0335d68bf451797aa4d89a7d0e784d7ac
Maxim Kleverov <maximkleverov@icloud.com>
2026-07-29 13:26:14 +0300

    working on mariadb

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -4,26 +4,41 @@ set -e
 
 echo "Launching MariaDB setup..."
 
+
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB..."
 
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
     # Start temporary server
+    mariadbd \
+        --user=mysql \
+        --datadir=/var/lib/mysql \
+        --skip-networking &
 
     i=0
     while [ "$i" -lt 20 ]; do
-        if mariadb-admin ping --silent; then
+        if mariadb-admin --socket=/run/mysqld/mysqld.sock ping --silent; then
+            echo "MariaDB is ready"
             break
         fi
-
         i=$((i + 1))
         sleep 1
     done
+    if [ "$i" -eq 20 ]; then
+        echo "Failed setting up MariaDB"
+        exit 1
+    fi
+
 
     # SQL comes here
 
     # Shutdown comes here
 fi
 
-# Start real server
+exec mariadbd \
+    --user=mysql \
+    --datadir=/var/lib/mysql
