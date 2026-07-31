c76b09984026ccc87a80ee7bc87cd4d9b87998a3
Maxim Kleverov <maximkleverov@icloud.com>
2026-07-31 23:12:21 +0300

    having issues with db

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -8,6 +8,8 @@ echo "Launching MariaDB setup..."
 mkdir -p /run/mysqld
 chown mysql:mysql /run/mysqld
 
+
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB..."
 
@@ -18,6 +20,8 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         --user=mysql \
         --datadir=/var/lib/mysql \
         --skip-networking &
+
+    DB_PID=$!
 
     i=0
     while [ "$i" -lt 20 ]; do
@@ -33,10 +37,30 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         exit 1
     fi
 
+echo "Running SQL..."
+    mariadb --socket=/run/mysqld/mysqld.sock <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 
-    # SQL comes here
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%'
+IDENTIFIED BY '${MYSQL_PASSWORD}';
 
-    # Shutdown comes here
+GRANT ALL PRIVILEGES
+ON \`${MYSQL_DATABASE}\`.*
+TO '${MYSQL_USER}'@'%';
+
+ALTER USER 'root'@'localhost'
+IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+FLUSH PRIVILEGES;
+EOF
+echo "SQL finished."
+
+    mariadb-admin \
+    --socket=/run/mysqld/mysqld.sock \
+    shutdown
+
+wait "$DB_PID"
+
 fi
 
 exec mariadbd \
