e6ddfbc7658fbaa1fc7da6b00351089b9c361ee7
Maxim Kleverov <maximkleverov@icloud.com>
2026-08-01 22:00:50 +0300

    wordpress

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -5,7 +5,11 @@ set -e
 echo "Launching Wordpress setup..."
 
 
+
 DB_PASSWORD=$(cat /run/secrets/db_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
+echo "Secrets loaded."
+
+exec php-fpm84 -F
